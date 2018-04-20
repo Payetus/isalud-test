216fccb70e0487626c76c00aecd8cd2e12cc2e7f
PayetusKid <payet91@gmail.com>
2018-04-20 13:48:05 +0200

    Implementing Command

--- app/Console/Commands/UpdateClients.php
+++ app/Console/Commands/UpdateClients.php
@@ -3,6 +3,12 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use App\Client;
+use GuzzleHttp\Client as HttpClient;
+use App\Factories\ClientFactory;
+use App\Parsers\JsonParser;
+use App\Parsers\XmlParser;
+use App\Serializers\CsvSerializer;
 
 class UpdateClients extends Command
 {
@@ -37,6 +43,36 @@ class UpdateClients extends Command
      */
     public function handle()
     {
-        //
+        $httpClient = new HttpClient();
+        $factory = new ClientFactory();
+        // Obtain data from api
+        $this->line('Obtaining data from internal api');
+        $res = $httpClient->get('https://jsonplaceholder.typicode.com/users');
+        $apiClients = [];
+        if ($res->getStatusCode() == 200) {
+            $json = $res->getBody();
+            $this->info('OK response');
+            // Parse data to clients array
+            $parser = new JsonParser();
+            $apiClients = $parser->parse($json, $factory);
+            $this->info('Obtained '.count($apiClients).' from api');
+        }else{
+            $this->error('Failed to obtain data');
+        }
+        // Obtain data from xml file
+        $this->line('Obtaining data from file');
+        // Parse data to clients
+        $xml = file_get_contents("data.xml");
+        $parser = new XmlParser;
+        $fileClients = $parser->parse($xml, $factory);
+        $this->info('Obtained '.count($fileClients).' from file');
+        // join data
+        $clients = array_merge($apiClients, $fileClients);
+        $this->info('Serializing to file');
+        // serialize to csv
+        $serializer = new CsvSerializer;
+        $path = "result.csv";
+        $serializer->serializeToFile($path, $clients);
+
     }
 }
